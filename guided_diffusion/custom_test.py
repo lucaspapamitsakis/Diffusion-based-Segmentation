59996ab052fd28efcb969abbc9309bae521fd094
Lucas Papamitsakis <lucaspapamitsakis@gmail.com>
2025-07-11 19:24:06 -0400

    cleaning up loader file

--- guided_diffusion/custom_test.py
+++ guided_diffusion/custom_test.py
@@ -1,3 +1,6 @@
+import sys
+sys.path.append("..")
+sys.path.append(".")
 import torch
 import torch.nn
 import numpy as np
@@ -5,72 +8,42 @@ import os
 import os.path
 import nibabel
 from torchvision.transforms import Resize
-# from monai.data import Dataset, DataLoader
-# from monai.transforms import (
-#     Compose,
-#     LoadImaged,
-#     Resized,
-#     ScaleIntensityd,
-#     SqueezeDimd,
-#     RandRotated,
-#     RandZoomd,
-#     RandFlipd,
-#     EnsureChannelFirstd,
-#     EnsureTyped,
-#     RandRotate90d,
-#     ResizeWithPadOrCropd
-# )
-
-# root = "../data"
-# split = "testing"
-# directory = os.path.join(root_dir, split)
-
-directory = os.path.expanduser('../data')
-database = []
-
-for root, dirs, files in os.walk(directory):
-    # print(root, dirs, files)
-    # print("-"*20)
-    # if there are no subdirs, we have data
-    if not dirs:
-        mr_files = sorted([f for f in files if f.startswith('mr')])
-        seg_files = sorted([f for f in files if f.startswith('seg')])
-
-        for mr_file in mr_files:
-            # Construct the corresponding segmentation file name by replacing 'mr' with 'seg'
-            seg_file = 'seg' + mr_file[2:]
-            if seg_file in seg_files:
-                database.append({'mr': os.path.join(root, mr_file), 'seg': os.path.join(root, seg_file)})
-
-print(database)
+from guided_diffusion.customloader import MRBoneDataset
+from guided_diffusion.bratsloader import BRATSDataset
 
 
+dat_path = "../data"
+dat_path2 = "../data_eg"
+# ds = MRBoneDataset(dat_path)
+ds = BRATSDataset(dat_path2)
+datal= torch.utils.data.DataLoader(
+    ds,
+    batch_size=1,
+    shuffle=True)
+data = iter(datal)
 
-datapoint = database[0]
-mr_path = datapoint["mr"]
-bone_path = datapoint["seg"]
+for batch in data:
+    print(type(batch))
+    break
 
-# Load the 3D images
-mr_img_nii = nibabel.load(mr_path)
-bone_img_nii = nibabel.load(bone_path)
 
-mr_img_data = torch.tensor(mr_img_nii.get_fdata(), dtype=torch.float32)
-bone_img_data = torch.tensor(bone_img_nii.get_fdata(), dtype=torch.float32)
-
-processed_slices = []
-for i in range(mr_img_data.shape[2]): 
-    mr_slice = mr_img_data[:, :, i].unsqueeze(0)
-    bone_slice = bone_img_data[:, :, i].unsqueeze(0)
-
-    resize_transform = Resize((219, 252), antialias=True)
-    mr_slice_resized = resize_transform(mr_slice)
-    bone_slice_resized = resize_transform(bone_slice)
-    
-    mr_slice_resized = (mr_slice_resized - mr_slice_resized.min()) / \
-                        (mr_slice_resized.max() - mr_slice_resized.min())
-    
-    bone_slice_resized = torch.where(bone_slice_resized > 0, 1.0, 0.0)
-    
-    processed_slices.append((mr_slice_resized, bone_slice_resized))
-    
-print(len(processed_slices))
+# logger.log("training...")
+# TrainLoop(
+#     model=model,
+#     diffusion=diffusion,
+#     classifier=None,
+#     data=data,
+#     dataloader=datal,
+#     batch_size=args.batch_size,
+#     microbatch=args.microbatch,
+#     lr=args.lr,
+#     ema_rate=args.ema_rate,
+#     log_interval=args.log_interval,
+#     save_interval=args.save_interval,
+#     resume_checkpoint=args.resume_checkpoint,
+#     use_fp16=args.use_fp16,
+#     fp16_scale_growth=args.fp16_scale_growth,
+#     schedule_sampler=schedule_sampler,
+#     weight_decay=args.weight_decay,
+#     lr_anneal_steps=args.lr_anneal_steps,
+# ).run_loop()
